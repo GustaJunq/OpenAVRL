f7021755d4bb1270a666a17d812c3aed7e8f0551
GustaJunq <128104845+GustaJunq@users.noreply.github.com>
2026-08-16 17:43:05 -0300

    Add Refiner to module exports

--- openavrl/__init__.py
+++ openavrl/__init__.py
@@ -1,4 +1,5 @@
 from .pipeline import OpenAVRL2Pipeline
+from .refiner import Refiner
 from .schemas import Critique, GenerationResult
 
-__all__ = ["OpenAVRL2Pipeline", "Critique", "GenerationResult"]
+__all__ = ["OpenAVRL2Pipeline", "Refiner", "Critique", "GenerationResult"]
